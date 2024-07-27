d8cccca710a5cc4249eb330e048c7a274a8b7980
Bhuvansh Goyal <bhuvanshgoyal2004@gmail.com>
2024-07-27 18:42:18 +0530

    weird algorithm

---
+++ src/weird_algorithm.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  do {
+    cout << n << " ";
+
+    if (n % 2 == 0) {
+      n /= 2;
+    } else {
+      n = 3 * n + 1;
+    }
+  } while (n != 1);
+  cout << 1 << endl;
+}
