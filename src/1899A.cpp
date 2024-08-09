09e6eeb01f494145e75a2387d8b1ceb885e26c8b
Bhuvansh Goyal <bhuvanshgoyal2004@gmail.com>
2024-08-10 01:51:43 +0530

    Added question: 1899A

---
+++ src/1899A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+    int n;
+    cin >> n;
+
+    if (n % 3 == 0) {
+      cout << "Second\n";
+    } else {
+      cout << "First\n";
+    }
+  }
+
+  return 0;
+}
