9ec0c29cccdd7b5f9361167d18c5ce685b12d6e6
Bhuvansh Goyal <bhuvanshgoyal2004@gmail.com>
2024-08-11 13:01:13 +0530

    Better approach for Trailing Zeros

--- src/trailing_zeros.cpp
+++ src/trailing_zeros.cpp
@@ -7,27 +7,13 @@ int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  ll n;
+  int n;
   cin >> n;
 
-  int five_count = 0;
-
-  short int *arr = new short int[n / 5];
-  int index = 0;
-
-  for (ll i = 5; i <= n; i += 5) {
-    ll remaining = i / 5;
-
-    if (remaining % 5 == 0) {
-      int count = 1 + arr[(remaining / 5) - 1];
-      arr[index++] = count;
-      five_count += count;
-    } else {
-      arr[index++] = 1;
-      five_count++;
-    }
+  int pow = 0;
+  for (int i = 5; i <= n; i *= 5) {
+    pow += n / i;
   }
 
-  cout << five_count << "\n";
-  delete[] arr;
+  cout << pow << "\n";
 }
