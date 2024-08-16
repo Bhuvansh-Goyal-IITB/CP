f787df66960f80119093b884fecbc1527b3330bb
Bhuvansh Goyal <bhuvanshgoyal2004@gmail.com>
2024-08-16 18:29:36 +0530

    Another approach to Maximum Subarray Sum

--- src/maximum_subarray_sum.cpp
+++ src/maximum_subarray_sum.cpp
@@ -3,23 +3,6 @@ using namespace std;
 
 #define ll long long
 
-void generate_dp_array(vector<int> &arr, vector<ll> &dp, int depth) {
-  int size = arr.size();
-
-  if (depth == size - 1) {
-    dp[depth] = arr[depth];
-    return;
-  }
-
-  generate_dp_array(arr, dp, depth + 1);
-
-  if (dp[depth + 1] > 0) {
-    dp[depth] = arr[depth] + dp[depth + 1];
-  } else {
-    dp[depth] = arr[depth];
-  }
-}
-
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
@@ -36,9 +19,18 @@ int main() {
     arr.push_back(val);
   }
 
-  vector<ll> dp(arr.size());
+  ll sum = 0;
+  ll ans = INT_MIN;
 
-  generate_dp_array(arr, dp, 0);
+  for (int val : arr) {
+    if (sum + val < 0) {
+      sum = 0;
+      if (val > ans) ans = val;
+    } else {
+      sum += val;
+      ans = max(sum, ans);
+    }
+  }
 
-  cout << *max_element(dp.begin(), dp.end()) << "\n";
+  cout << ans << "\n";
 }
