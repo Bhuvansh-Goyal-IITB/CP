d81793b7b52eb2a31d09119a4f52e9210c1e8ee4
Bhuvansh Goyal <bhuvanshgoyal2004@gmail.com>
2024-08-18 01:33:55 +0530

    Added question: 1845A

--- src/1845A.cpp
+++ src/1845A.cpp
@@ -11,5 +11,55 @@ int main() {
   cin >> t;
 
   while (t--) {
+    int n, k, x;
+    cin >> n >> k >> x;
+
+    if (x == 1) {
+      if (k == 1) {
+        cout << "NO\n";
+      } else if (k == 2) {
+        if (n & 1) {
+          cout << "NO\n";
+        } else {
+          cout << "YES\n";
+
+          cout << n / 2 << "\n";
+          for (int i = 0; i < n / 2; i++) {
+            cout << 2 << " ";
+          }
+          cout << "\n";
+        }
+      } else {
+        cout << "YES\n";
+
+        if (n == 4) {
+          cout << 2 << "\n";
+          cout << 2 << " " << 2 << "\n";
+        } else {
+          if (n & 1) {
+            int sum = 3;
+            cout << 1 + ((n - sum) / 2) << "\n";
+            cout << 3 << " ";
+            while (sum != n) {
+              sum += 2;
+              cout << 2 << " ";
+            }
+          } else {
+            cout << n / 2 << "\n";
+            for (int i = 0; i < n / 2; i++) {
+              cout << 2 << " ";
+            }
+          }
+          cout << "\n";
+        }
+      }
+    } else {
+      cout << "YES\n";
+      cout << n << "\n";
+      for (int i = 0; i < n; i++) {
+        cout << 1 << " ";
+      }
+      cout << "\n";
+    }
   }
 }
