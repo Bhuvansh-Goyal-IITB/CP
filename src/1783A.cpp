e0b49ee0dddea6cf3f41bd72bd2b2c6cffa8232c
Bhuvansh Goyal <bhuvanshgoyal2004@gmail.com>
2024-08-19 15:26:14 +0530

    Another approach to 1783A

--- src/1783A.cpp
+++ src/1783A.cpp
@@ -15,48 +15,22 @@ int main() {
     cin >> n;
 
     vector<int> arr(n);
-    vector<int> sum_arr(n);
 
     for (int i = 0; i < n; i++) {
       cin >> arr[i];
-
-      if (i == 0) {
-        sum_arr[i] = arr[i];
-      } else {
-        sum_arr[i] = sum_arr[i - 1] + arr[i];
-      }
     }
 
-    bool possible = true;
-
-    for (int i = 1; i < n; i++) {
-      if (arr[i] == sum_arr[i - 1]) {
-        if (arr[i] != arr[i - 1]) {
-          swap(arr[i], arr[i - 1]);
-        } else {
-          if (arr[i] == arr[n - 1]) {
-            possible = false;
-            break;
-          }
-
-          int ci = i;
-
-          while (i < n && arr[i] == arr[ci]) i++;
-
-          swap(arr[ci], arr[i]);
-        }
-      }
-    }
-
-    if (possible) {
-      cout << "YES\n";
-      for (int val : arr) {
-        cout << val << " ";
-      }
-      cout << "\n";
-    } else {
+    if (arr[0] == arr[n - 1]) {
       cout << "NO\n";
+      continue;
     }
+
+    cout << "YES\n";
+    cout << arr[0] << " ";
+    for (int i = n - 1; i >= 1; i--) {
+      cout << arr[i] << " ";
+    }
+    cout << "\n";
   }
 
   return 0;
